fb08327cb001018aff15d30cfe21c10ee249fb49
kunal kursija <kunal_k66@yahoo.com>
2021-05-06 19:11:00 +0530

    Adding Initial Functionality

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # cowin_vaccine_notifier
-PHP package to search COVID vaccine availability in India by pin-codes
+- PHP package to search COVID vaccine availability in India by pin-codes in every one minute.
+
+##Pre-Requisites:
+- Make sure you have PHP installed on your system.
+- To check if its installed or not, Execute below command from the command line tool:
+```bash
+php --version
+```
+
+###Introduction
+- This package helps you in finding the available vaccines on https://selfregistration.cowin.gov.in/ by Pin-Codes.
+ 
+###Installation
+- Open Command Line Tool and clone the repository by executing below command:
+```bash
+git clone git@github.com:kunal-kursija/cowin_vaccine_notifier.git
+```
+
+###Usage
+- Go to the newly cloned directory and edit the file `cowin_notifier.php`.
+- Configure Pin-Codes that you want to search on Line.No.9. And they need to be in comma separated format.
+  Example: `$pincodes = [400001, 421004, 431127, 431523, 413249, 431131, 414201, 416810];`
+- Configure Age Group for which you want to search vaccines on Line.No.18.
+  Example:  `$age = 18;` OR `$age = 45;`
+- Change the directory to the newly cloned directory by following command:
+```bash
+cd cowin_vaccine_notifier
+```
+- Execute the php script by below command:
+```bash
+php -f cowin_notifier.php
+```
+
+- The script will execute every one minute and will shout "Vaccines Found" if it comes across any available slots in your configured pincodes.
+- If you want to change the lookup frequency, You can change it from Line.No.54 as `sleep(60);` where 60 is the number of seconds. By default we execute the script every 1 minute.
---
+++ cowin_notifier.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @file
+ * Checks if vaccinations are available near a pin code in India.
+ */
+
+// Search for vaccines at below pincodes.
+$pincodes = [400001, 421004, 431127, 431523, 413249, 431131, 414201, 416810];
+// Vaccine Age Group.
+$age = 18;
+// Search For Vaccines starting from today's date.
+$date = date('d-m-Y');
+
+
+set_time_limit(0);
+while (TRUE) {
+  $output = [];
+  // Assume no vaccines are found.
+  $vaccines_found = FALSE;
+  // Lookup Vaccine at below Pin-codes.
+  foreach ($pincodes as $pincode) {
+    $response = json_decode(get_availability($pincode, $date), JSON_HEX_QUOT);
+
+    // Loop through the centers having vaccine.
+    if (count($response) > 0 && count($response['centers']) > 0) {
+      foreach ($response['centers'] as $center) {
+        foreach ($center['sessions'] as $session) {
+          if ($session['available_capacity'] > 0 && $session['min_age_limit'] == $age) {
+            $vaccines_found = TRUE;
+            $output[] = $pincode . " - FOUND | " . $session['date'] . ' | AGE: ' . $session['min_age_limit'] . '+ | ' . 'Quantity: ' . $session['available_capacity'] . ' | '. $center['name'] . ', ' . $center['address'] . ' | ' . $center['block_name'] . ' | ' . $center['district_name'] . ' | ' . $center['state_name'];
+          }
+        }
+      }
+    }
+  }
+
+  // Let your system say it loud.
+  if ($vaccines_found) {
+    // Output the result on CLI.
+    print_r(implode("\n", $output));
+    print_r("\n");
+    print_r("----------------------------------- REGISTER HERE : https://selfregistration.cowin.gov.in/ -----------------------------------");
+    print_r("\n");
+    exec("say 'Vaccines Found'");
+  }
+  else {
+    print_r("\n");
+    print_r("--------------- Could Not Find Any Vaccines ---------------");
+    print_r("\n");
+  }
+
+  // Search Every 60 Seconds.
+  sleep(60);
+}
+
+/**
+ * Helper function to check availability of a vaccine at a particular pin-code on provided date.
+ */
+function get_availability($pincode, $date) {
+  $url = get_url($pincode, $date);
+  $command = "curl -s -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36' '$url'";
+  return exec($command);
+}
+
+/**
+ * Helper function to create API url.
+ */
+function get_url($pincode, $date) {
+  return 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=' . $pincode .'&date=' . $date;
+}
